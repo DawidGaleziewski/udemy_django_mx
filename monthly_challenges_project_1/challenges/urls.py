6f77d886b1fceda4addf97c83684d04632073af4
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-08-26 22:50:41 +0200

    update: learning about views and routing in django

---
+++ monthly_challenges_project_1/challenges/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+# URL config. Needs to be connect to url config. Requests will be routed here from main url config
+urlpatterns = [
+    path("january", views.index)
+]
--- monthly_challenges_project_1/monthly_challenges_project_1/urls.py
+++ monthly_challenges_project_1/monthly_challenges_project_1/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# main url config. This routes requests into app specific url paths
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # this defines route challaenges and passes url config in second param
+    path('challenges/', include("challenges.urls"))
 ]
