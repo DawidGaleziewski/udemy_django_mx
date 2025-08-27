8dfa6a8a1e2f7318ac78a2022db625b237ca857f
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-08-27 19:47:12 +0200

    save notes

--- monthly_challenges_project_1/challenges/urls.py
+++ monthly_challenges_project_1/challenges/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # URL config. Needs to be connect to url config. Requests will be routed here from main url config
 urlpatterns = [
-    path("january", views.index)
+    path("january", views.january),
+    path("feb", views.feb)
 ]
--- monthly_challenges_project_1/challenges/views.py
+++ monthly_challenges_project_1/challenges/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("This works")
+def january(request):
+    return HttpResponse("January")
+
+
+def feb(request):
+    return HttpResponse("February")
