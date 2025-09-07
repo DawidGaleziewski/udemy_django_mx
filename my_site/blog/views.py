aa99536a25adaccf6e82a9aeddc360b30464270c
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-09-07 19:16:22 +0200

    update: add styling to blog post

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from dataclasses import dataclass
 
@@ -7,18 +9,22 @@ class BlogPost:
     content: str
     id: int
     slug: str
-    def __init__(self, title, content, id):
+    author: str
+    date: datetime.datetime
+    def __init__(self, title, content, id, author):
         self.title = title
         self.content = content
         self.id = id
         self.slug = title.lower().replace(" ", "-")
+        self.date = datetime.datetime.now()
+        self.author = author
 
 
 posts: list[BlogPost] = [
-    BlogPost("I love onion", "Humble yet bold, the onion is flavor’s quiet architect. Raw slices add bite; slow sweats melt into sweetness; roasting coaxes caramel depths. Reds brighten salads, yellows anchor soups and sauces, sweets sing on the grill. Cheap, storable, and rich in prebiotic fiber and quercetin, onions deserve a permanent pantry spot. Pro tip: leave the root intact until the end to tame tears. With time and heat, this layered bulb turns simple food into comfort. Peel back a layer, and dinner possibilities multiply.", 1),
-    BlogPost("My Second Blog Post", "This is my second blog post", 2),
-    BlogPost("My Third Blog Post", "This is my third blog post", 3),
-    BlogPost("My Fourth Blog Post", "This is my fourth blog post", 4),
+    BlogPost("I love onion", "Humble yet bold, the onion is flavor’s quiet architect. Raw slices add bite; slow sweats melt into sweetness; roasting coaxes caramel depths. Reds brighten salads, yellows anchor soups and sauces, sweets sing on the grill. Cheap, storable, and rich in prebiotic fiber and quercetin, onions deserve a permanent pantry spot. Pro tip: leave the root intact until the end to tame tears. With time and heat, this layered bulb turns simple food into comfort. Peel back a layer, and dinner possibilities multiply.", 1, 'j4ch'),
+    BlogPost("My Second Blog Post", "This is my second blog post", 2, 'j4ch'),
+    BlogPost("My Third Blog Post", "This is my third blog post", 3, 'j4ch'),
+    BlogPost("My Fourth Blog Post", "This is my fourth blog post", 4, 'j4ch'),
 ]
 
 # Create your views here.
