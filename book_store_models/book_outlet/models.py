60286ad93b3487af8e18c56e9ea45e54be1d9c51
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-09-09 19:49:53 +0200

    update: learned on models and manging migrations in django

--- book_store_models/book_outlet/models.py
+++ book_store_models/book_outlet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 # define data entities here
@@ -8,6 +9,10 @@ class Book(models.Model):
     # all oficialy suported field types: https://docs.djangoproject.com/en/5.2/ref/models/fields/
     # id is created automatically for each entry
     title = models.CharField(max_length=100)
-    rating = models.IntegerField()
-
-
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(max_length=100, null=True)
+    is_bestseller = models.BooleanField(default=False)
+    # this defines how things are stringified for object. Similar to java
+    # if we add a method we dont need to run migration again. Only if we change the structure of the db fsdfsdf sdfsdf sdf sdfsdfsd fsdf sdf sdf sdf sdf sdf sd
+    def __str__(self):
+        return f"{self.title} - {self.rating} - {self.author}"
--- book_store_models/notes.md
+++ book_store_models/notes.md
@@ -21,3 +21,29 @@ To apply all migrations that have not been applied yet:
 ```bash
  py .\manage.py migrate
 ```
+
+# interactive shell and saving a entry
+you can open shell that will have access to your apps by running:
+```bash
+py .\manage.py shell
+```
+
+after that we can do things like play with our models
+```python
+from book_outlet.models import Book
+hp = Book(title="Hary Pyta", rating=2) # creates a new object using model
+hp.save() # saves the model to the database
+```
+
+
+# interactive shell and reading the data
+you can open shell that will have access to your apps by running:
+
+after that we can do things like play with our models
+```python
+from book_outlet.models import Book
+Book.objects.all() # gets all saved objects
+```
+
+# migrations on existing data
+if we run migrations, and new data was added, but some data already exists. We will have to choose to eaither provide a one-off default or to add a default in migrations
