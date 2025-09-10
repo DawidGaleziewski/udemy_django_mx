a811f3fdac158335036ca80b0697d1e6bd443b6f
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-09-10 23:28:10 +0200

    update: using model to get data from db

--- book_store_models/book_outlet/urls.py
+++ book_store_models/book_outlet/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from book_outlet import views
 
 urlpatterns = [
-    path('', views.index, name='book_store'),
+    path('', views.index, name='book-store'),
+    path('<int:book_id>', views.book_detail, name='book-detail'),
 ]
--- book_store_models/book_outlet/views.py
+++ book_store_models/book_outlet/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from book_outlet import models
+from book_outlet.models import Book
 
 # Create your views here.
 def index(request):
-    all_books = models.Book.objects.all()
+    all_books = Book.objects.all()
     return render(request, "index.html", {"books": all_books})
+
+def book_detail(request, book_id:int):
+    book = Book.objects.get(id=book_id)
+    return render(request, "book_detail.html", {"book": book})
