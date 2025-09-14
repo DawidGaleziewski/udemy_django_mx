a397cd8cb0494c3003d8c762a6e6114e2e45d48f
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-09-14 13:57:05 +0200

    saving notes

--- book_store_models/book_outlet/models.py
+++ book_store_models/book_outlet/models.py
@@ -12,6 +12,9 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Countries"
+
 # Create your models here.
 # define data entities here
 
