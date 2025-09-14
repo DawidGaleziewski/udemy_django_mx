434ee0138ff229b6dd6abdafcee015a3bd40cdd0
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-09-14 23:15:18 +0200

    save notes

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Post, Author, Tag
 
+
+class PostTagInline(admin.TabularInline):
+    model = Tag.posts.through
+    extra = 1
+    verbose_name = "Tag"
+    verbose_name_plural = "Tags"
+
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    inlines = [PostTagInline]
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
