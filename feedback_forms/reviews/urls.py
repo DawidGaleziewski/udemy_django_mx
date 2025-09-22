899bf038960177958c2e13a19ee645da549bbb02
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-09-22 20:31:31 +0200

    update: saving notes

--- feedback_forms/reviews/urls.py
+++ feedback_forms/reviews/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='reviews'),
+    path('class-view', views.ReviewView.as_view(), name='reviews-class'),
     path("thank-you", views.thank_you, name="thank-you")
 ]
--- feedback_forms/reviews/views.py
+++ feedback_forms/reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm2
 from .models import Review
+from django.views import View
 
 # Create your views here.
 
@@ -13,12 +14,14 @@ def index(request):
 
         # is_valid will validate inputs. But also populate clean data with values
         if form.is_valid():
-            new_review = Review(
-                name = form.cleaned_data["name"],
-                review = form.cleaned_data["review"],
-                rating = form.cleaned_data["rating"]
-            )
-            new_review.save()
+            # new_review = Review(
+            #     name = form.cleaned_data["name"],
+            #     review = form.cleaned_data["review"],
+            #     rating = form.cleaned_data["rating"]
+            # )
+            # new_review.save()
+            # when using model form. We can simply save right to db here
+            form.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm2()
@@ -31,3 +34,29 @@ def index(request):
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')
+
+
+# some views can be defined as class
+# convention is to add View but we dont have to
+# we extand View class
+class ReviewView(View):
+    # class allows us to define different methods for the same view
+    def get(self, request):
+        form = ReviewForm2()
+        reviews = Review.objects.all()
+        return render(request, 'reviews/reviews.html', {
+            "form": form,
+            "reviews": reviews
+        })
+
+    def post(self, request):
+        form = ReviewForm2(request.POST)
+        reviews = Review.objects.all()
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+
+        return render(request, 'reviews/reviews.html', {
+            "form": form,
+            "reviews": reviews
+        })
