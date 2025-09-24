762aa085440b8e996748767b339b486d45edf318
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-09-24 22:53:08 +0200

    update: using templateView

--- class_views/abstracted/urls.py
+++ class_views/abstracted/urls.py
@@ -1,6 +1,7 @@
-from .views import IndexView
+from .views import IndexView, RevDetail
 from django.urls import path
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('detail/<id>', RevDetail.as_view(), name='detail')
 ]
--- class_views/abstracted/views.py
+++ class_views/abstracted/views.py
@@ -14,3 +14,15 @@ class IndexView(TemplateView):
         reviews = Review.objects.all()
         context["reviews"] = reviews
         return context
+
+
+class RevDetail(TemplateView):
+    template_name = "details.html"
+    model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        review = Review.objects.get(id=review_id)
+        context["review"] = review
+        return context
