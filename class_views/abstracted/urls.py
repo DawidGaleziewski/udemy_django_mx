442e22dbedc2cbaef3d587b5f72d0c6856f5b5ea
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-09-25 20:25:50 +0200

    update: Detail and List views

--- class_views/abstracted/urls.py
+++ class_views/abstracted/urls.py
@@ -1,7 +1,8 @@
-from .views import IndexView, RevDetail
+from .views import ReviewListClass, RevDetail
 from django.urls import path
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('detail/<id>', RevDetail.as_view(), name='detail')
+    path('', ReviewListClass.as_view(), name='index'),
+    # it is important to name this eaither pk or slug, for django DetailView to query by this param
+    path('detail/<pk>', RevDetail.as_view(), name='detail')
 ]
--- class_views/abstracted/views.py
+++ class_views/abstracted/views.py
@@ -1,28 +1,39 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Review
 
 # TemplateView allows us to define a template that will be used
-class IndexView(TemplateView):
-    template_name = "abstracted.html"
+# class IndexView(TemplateView):
+#     template_name = "abstracted.html"
+#
+#     # constructing context provided to the template
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["title"] = "Django Class Views"
+#         reviews = Review.objects.all()
+#         context["reviews"] = reviews
+#         return context
 
-    # constructing context provided to the template
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = "Django Class Views"
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
 
-
-class RevDetail(TemplateView):
+# DetailView class will use pk or 'slug' dynamic route param to get the data
+class RevDetail(DetailView):
     template_name = "details.html"
+    context_object_name = "review"
     model = Review
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs["id"]
-        review = Review.objects.get(id=review_id)
-        context["review"] = review
-        return context
+# one thing to remember is that in temlate this will be shown as object_list
+class ReviewListClass(ListView):
+    model = Review
+    template_name = "abstracted.html"
+    # we can however change object list to a diffrent name
+    context_object_name = "reviews"
+
+    # if we however want to filter some of the items, we can do it here
+    def get_queryset(self):
+        base_queryset = super().get_queryset()
+        # we could override it here
+        # base_queryset.filter(rating__gte=4)
+        return base_queryset
